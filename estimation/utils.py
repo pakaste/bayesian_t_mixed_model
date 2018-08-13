ef5a89a4ee28ef6bd4ad4d9fb7243b24eda5bbf3
paka <pauliina.karell@topdatascience.com>
2018-08-13 09:00:02 +0300

    fixed estimation of sigma_u

--- estimation/utils.py
+++ estimation/utils.py
@@ -1,20 +1,32 @@
 import numpy as np
 
 
-def calculate_covariance_u(Z, u, sigma_u, nu_u):
+def calculate_uAu(Z, u):
     Z_tranpose = np.transpose(Z)
     A = np.dot(Z_tranpose, Z)
+    A_inv = np.linalg.inv(A)
 
     # Multiply by u
-    S_u = np.dot(np.dot(np.transpose(u), A), u)
+    covariance = np.dot(np.dot(np.transpose(u), A_inv), u)
+
+    return covariance
+
+
+def calculate_covariance_u(Z, u, sigma_u, nu_u):
+    S_u = calculate_uAu(Z, u)
     scaled_S_u = (S_u / (1 / sigma_u)) + nu_u
 
     return scaled_S_u
 
 
 def calculate_sse(y, X, b, Z, u):
-    sse = y - np.dot(X, b) - np.dot(Z, u)
-    sse_tranpose = np.transpose(sse)
+
+    Xb = np.dot(X, b)
+    Zu = np.dot(Z, u)
+
+    # SSE^T SSE
+    sse = y - Xb - Zu
+    sse_transpose = np.transpose(sse)
 
     # Take the matrix multiplication
     dot_product = np.dot(sse_transpose, sse)
@@ -26,8 +38,6 @@ def calculate_S_e(sigma_e, nu_e, y, X, b, Z, u):
 
     sse = calculate_sse(y, X, b, Z, u)
     scaled_sse = (sse / sigma_e) + nu_e
-
-    final_sum = np.sum()
 
     return scaled_sse
 
@@ -44,7 +54,7 @@ def calculate_sum(s, y, X, b, Z, u):
         Returns:
             (int)
     """
-    sse = calculate_sse(y, X, b, Z, u)
-    S_t = np.sum(s * np.transpose(sse) * sse)
+    dot_product = calculate_sse(y, X, b, Z, u)
+    S_t_m = s * dot_product
 
-    return S_t
+    return S_t_m
--- estimation/variance_parameters.py
+++ estimation/variance_parameters.py
@@ -1,6 +1,7 @@
 from numpy.random import chisquare
 import numpy as np
 
+from estimation.utils import calculate_uAu
 from estimation.utils import calculate_covariance_u
 from estimation.utils import calculate_sum
 from estimation.utils import calculate_S_e
@@ -17,23 +18,16 @@ def estimate_s_e(sigma_e, nu_e, y, X, b, Z, u, family_indices):
 
     for ind in family_indices:
         df = nu_e + len(ind)
+        y_copy = np.squeeze(y.copy())
         X_copy = X.copy()
-        b_copy = b.copy()
         Z_copy = Z.copy()
-        u_copy = u.copy()
 
         # Calculate the S_e for every other observation, except belonging to this group
+        y_subset = np.delete(y_copy, ind, axis=0)
         X_subset = np.delete(X_copy, ind, axis=0)
         Z_subset = np.delete(Z_copy, ind, axis=0)
-        b_subset = np.delete(b_copy, ind, axis=0)
-        u_subset = np.delete(u_copy, ind, axis=0)
 
-        print('X_subset ', X_subset.shape)
-        print('b_subset ', b_subset.shape)
-        print('Z_subset ', Z_subset.shape)
-        print('u_subset ', u_subset.shape)
-
-        S_e = calculate_S_e(sigma_e, nu_e, y, X_subset, b_subset, Z_subset, u_subset)
+        S_e = calculate_S_e(sigma_e, nu_e, y_subset, X_subset, b, Z_subset, u)
 
         # Calculate the estimate for the family i
         s_e_i = chisquare(df) / S_e
@@ -58,11 +52,36 @@ def estimate_s_u(Z, u, sigma_u, nu_u):
     return s_u_estimate
 
 
-def estimate_sigma_e(s, y, X, b, Z, u, tau_e, Tau_e):
-    S_t = calculate_sum(s, y, X, b, Z, u)
+def estimate_sigma_e(s_e, y, X, b, Z, u, tau_e, Tau_e, family_indices):
+    """
+        Updates the estimate for sigma_e
+    """
+
+    S_t = 0
+    for family_ind in family_indices:
+
+        # Calculate the S_t for every group m separately
+        y_subset = np.squeeze(y[family_ind])
+        X_subset = X[family_ind]
+        Z_subset = Z[family_ind]
+        s_e_i = s_e[family_ind][0]
+
+        group_m_S_t = calculate_sum(s_e_i, y_subset, X_subset, b, Z_subset, u)
+        S_t = S_t + group_m_S_t
+
     nominator = (tau_e * Tau_e) + S_t
-    df = Tau_e + X.shape[0]
+    df = tau_e + X.shape[0]
 
     sigma_estimate = nominator / chisquare(df=df)
 
     return sigma_estimate
+
+
+def estimate_sigma_u(s_u, y, X, b, Z, u, tau_u, Tau_u):
+    covariance_u = calculate_uAu(Z, u)
+    nominator = (tau_u*Tau_u + s_u*covariance_u)
+
+    df = tau_u + Z.shape[1]
+    updated_sigma_u = nominator / chisquare(df=df)
+
+    return updated_sigma_u
