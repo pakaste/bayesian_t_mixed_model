cd1e7459a47e6c00aad3b9e2d2dc6bb5db3e2843
paka <pauliina.karell@topdatascience.com>
2018-08-13 09:00:14 +0300

    cleaned code

--- run_analysis.py
+++ run_analysis.py
@@ -12,8 +12,11 @@ from data_wrangling.training_testing_set import create_familial_matrix
 from visualization.plotting import plot_histogram
 from utils.hyperparameters import initialize_familial_parameters
 from utils.hyperparameters import initialize_individual_parameters
-from estimation.hendersson_equations import henderson_model_equations
 from estimation.blup_estimates import estimate_BLUP
+from estimation.variance_parameters import estimate_s_e
+from estimation.variance_parameters import estimate_s_u
+from estimation.variance_parameters import estimate_sigma_e
+from estimation.variance_parameters import estimate_sigma_u
 from settings import CONFIGS as cf
 
 
@@ -32,7 +35,7 @@ print('Shape of pheno data:', pheno.shape)
 
 
 # Subset data
-pheno_subset = subset_pheno(pheno, categorize=True, remove_correlated=True)
+pheno_subset = subset_pheno(pheno, categorize=False, remove_correlated=True)
 print('Shape of subset', pheno_subset.shape)
 print(pheno_subset.head())
 
@@ -43,37 +46,34 @@ print('X_train.shape, y_train.shape: ', X_train.shape, y_train.shape)
 print('X_test.shape, y_test.shape: ', X_test.shape, y_test.shape)
 
 # Calculate Z matrix
-Z_train = create_familial_matrix(X_train, X_train['family_nb'])
-print(Z_train)
-print('Amount of 0.98: ', np.count_nonzero(Z_train == 0.98))
-
+family_ids = list(set(X_train['family_nb']))
+Z_train, family_indices = create_familial_matrix(X_train, family_ids)
 print('Shape of Z_train: ', Z_train.shape)
 
 # Number of families
-n_families = len(list(set(X_train['family_nb'])))
+n_families = len(family_ids)
 print('Amount of families', n_families)
 
 # Drop family column and turn X into numpy array
-print(X_train.drop(columns='family_nb').columns)
-X_train = np.array(X_train.drop(columns='family_nb'))
+X_train = X_train.drop(columns='family_nb')
+X_train = np.array(X_train)
 y_train = np.array(y_train)
 
 
 print('\n########################################')
 print('Initializing hyperparameters')
-tau_b = 2.0
-Tau_b = 0.05
-nu_b = 3.0
-
+tau_b = 4.0
+Tau_b = 3/8
+nu_b = 2.0
 
 sigma_b, s_b, b = initialize_familial_parameters(n_fam=n_families, tau_b=tau_b, Tau_b=Tau_b, nu_b=nu_b)
 plot_histogram(b, 'Ranfom effects')
 
 
 # The inpendent individual error term
-tau_e = 1.0
-Tau_e = 0.01
-nu_e = 2.0
+tau_e = 4.0
+Tau_e = 1/8
+nu_e = 1.0
 
 sigma_e, s_e, scale_param = initialize_individual_parameters(n=X_train.shape[0], tau_e=tau_e, Tau_e=Tau_e, nu_e=nu_e)
 plot_histogram(s_e, 'Mixture parameter of individual error term')
@@ -83,15 +83,16 @@ plot_histogram(scale_param, 'Scale parameter')
 print('\n#####################################')
 print('START CALCULATING THE BLUP')
 
-p_dim = X_train.shape[1] + Z_train.shape[1]
-coef_estimates = np.array([0.0001]*p_dim)
+print('Average variance for b: ', np.mean(sigma_b))
+print('Average variance for e: ', np.mean(sigma_e))
 
-coefficient_matrix, right_hand = henderson_model_equations(y_train, X_train, Z_train, s_b, sigma_b, s_e, sigma_e)
-print('Size of coefficient matrix: ', coefficient_matrix.shape)
+final_estimates, updated_s_e, updated_s_u, updated_sigma_e, updated_sigma_b = estimate_BLUP(y_train, X_train, Z_train, s_b, sigma_b, tau_b, Tau_b, nu_b, s_e, sigma_e, tau_e, Tau_e, nu_e, family_indices, n=10000)
 
+for i in range(1, p_dim):
+    estimation = round(np.mean(final_estimates[1000:, i]), 5)
+    print('estimated params is {}'.format(estimation))
 
-#final_estimates = estimate_BLUP(coefficient_matrix=coefficient_matrix, coefficients=coef_estimates, right_hand=right_hand, n=200000)
-
-
+    plt.plot(final_estimates[2000:, i])
+    plt.show()
 
 
