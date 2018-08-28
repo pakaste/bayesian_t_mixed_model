1556ec7ecf3cc6f5f5bd485f1b2a84c8288a7857
paka <pauliina.karell@topdatascience.com>
2018-08-28 17:35:34 +0300

    added function to simulate fake data

--- data_wrangling/simulate_data.py
+++ data_wrangling/simulate_data.py
@@ -1,5 +1,52 @@
+import random
 import numpy as np
+from numpy.random import normal
 from sklearn.preprocessing import OneHotEncoder
+
+from utils.hyperparameters import initialize_familial_parameters
+from utils.hyperparameters import initialize_individual_parameters
+
+
+def simulate_fake_data(n, p, tau_b, Tau_b, nu_b, tau_e, Tau_e, nu_e, seed=10):
+
+    # Set the PRNG seed
+    random.seed(seed)
+
+    # Generate simulated X matrix
+    X = generate_data(n, p)
+    print('Shape simulated of X: ', X.shape)
+
+    # Generate simulated beta
+    beta = generate_parameters(p=p)
+    print('Shape simulated of beta: ', beta.shape)
+
+    # Create simulated Z matrix
+    n_families = int(n/2)
+    Z = np.zeros(shape=(n, n_families))
+    print('Shape simulated of Z', Z.shape)
+
+    # Fill Z matrix
+    j = 0
+    family_indices = []
+    for i in range(n_families):
+        family_indices.append([j, j+1])
+        Z[j:(j + 2), i] = np.array([1, 1])
+        j = j + 2
+
+    print('Initializing hyperparameters')
+
+    # Familial effects
+    sigma_b, s_b, b = initialize_familial_parameters(n_fam=n_families, tau_b=tau_b, Tau_b=Tau_b, nu_b=nu_b)
+
+    # The inpendent individual error term
+    sigma_e, s_e, scale_param = initialize_individual_parameters(family_indices=family_indices, n=n, tau_e=tau_e, Tau_e=Tau_e, nu_e=nu_e)
+
+    print('scale_param:' , scale_param)
+
+    print('Initializing dependent variable')
+    y = np.dot(X, beta) + np.dot(Z, b) + np.random.normal(loc=0, scale=scale_param, size=n)
+
+    return y, X, beta, Z, sigma_b, s_b, b, sigma_e, s_e, scale_param, family_indices
 
 
 def generate_data(n=100, p=10, categorical=False):
